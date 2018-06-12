653753e79c5ee242a96c9a6ce73a1d4d2e302ef4
Joie Cosby <jc283108@gmail.com>
2018-06-12 14:47:25 -0600

    Utilized get and saved info reloads

--- client/src/components/Goals.js
+++ client/src/components/Goals.js
@@ -16,12 +16,23 @@ class Goals extends React.Component {
     }
 
     this.submitButton = this.submitButton.bind(this);
-    this.updateButton = this.updateButton.bind(this);
     this.goalInput = this.goalInput.bind(this);
     this.updateGoal = this.updateGoal.bind(this);
     this.amountConsumedInput = this.amountConsumedInput.bind(this);
 
   }
+  componentDidMount() {
+    fetch('/api/dashboard',{
+      method: 'GET',
+      headers: {
+      Authorization: `bearer ${Auth.getToken()}`}})
+    .then ( ( res )  => {return res.json()})
+    .then (data => this.setState({
+      waterGoal: data.user.waterGoal,
+      totalAmountConsumed : data.user.totalAmountConsumed
+    }));
+  }
+
 
   submitButton(evt) {
     evt.preventDefault();
@@ -42,10 +53,6 @@ class Goals extends React.Component {
     .then (data => console.log(data))
 };
 
-  updateButton(e) {
-      console.log(typeof this.props.totalAmountConsumed);
-  }
-
   goalInput(e) {
     this.setState({waterGoal: e.target.value})
     console.log("waterGoal",this.state.waterGoal);
@@ -60,6 +67,7 @@ class Goals extends React.Component {
 
     this.setState({amountJustConsumed: e.target.value})
   }
+
   render() {
     const newTotal = this.state.totalAmountConsumed + this.state.amountJustConsumed;
     const amountLeft = this.state.waterGoal - this.state.totalAmountConsumed;
@@ -73,18 +81,12 @@ class Goals extends React.Component {
           label="waterGoals"
         />
       </form>
-      <RaisedButton onClick={this.submitButton} type="submit" label="Submit"/>
       <h3>How much water did you just drink?
       </h3>
       <form noValidate="noValidate" autoComplete="off">
         <TextField onChange={this.amountConsumedInput} id="updateWater" label="updateWater"/>
       </form>
-      <RaisedButton onClick={this.updateButton} type="submit" label="Update" color= 'primary'/>
-<<<<<<< HEAD
-
-
-=======
->>>>>>> f2723071239037eff13c536427ebceace1bdb681
+      <RaisedButton onClick={this.submitButton} type="submit" label="Submit"/>
       <h3>
         Your daily water goal is: {this.state.waterGoal} ounces</h3>
       <h3>
