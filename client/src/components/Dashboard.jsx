3a659ba332cff3c38931367f30339fc93888d002
Kate Stallbaumer <kate.stallbaumer@gmail.com>
2018-06-14 11:45:31 -0600

    cleaned up code for Homepage, Dashboard.jsx, Goals.jsx

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -4,36 +4,33 @@ import { Card, CardTitle, CardText} from 'material-ui/Card';
 import TextField from 'material-ui/TextField';
 import Goals from './Goals.js';
 import HistoryCal from './HistoryCal.jsx';
-// import styles from './Dashboard.css';
 
 const Dashboard = ({ secretData, user, history }) => (
-<div>
-  <div style={style.topheading} className="topHeading">
-    <Card>
-      <CardTitle
-        title="HalfFül"
-        subtitle="Drink all the waters."
-      />
-    </Card>
-  </div>
   <div>
-    <Card className="container">
-      <CardTitle/>
-    {secretData && <CardText style={{ fontSize: '16px', color: 'green' }}>Welcome <strong>{user.name}</strong>!<br /></CardText>}
-    <Goals
-        waterGoal = {user.waterGoal}
-        totalAmountConsumed = {user.totalAmountConsumed}
-        userId = {user._id}
-        history = {history}
-     />
-    </Card>
-    <Card className="container">
-      <CardTitle/>
-    </Card>
-
+    <div style={style.topheading} className="topHeading">
+      <Card>
+        <CardTitle
+          title="HalfFül"
+          subtitle="Drink all the waters."
+        />
+      </Card>
+    </div>
+    <div>
+      <Card className="container">
+          {secretData && <CardText style={{ fontSize: '16px', color: 'green' }}>Welcome <strong>{user.name}</strong>!<br /></CardText>}
+      <Goals
+          waterGoal = {user.waterGoal}
+          totalAmountConsumed = {user.totalAmountConsumed}
+          userId = {user._id}
+          history = {history}
+       />
+      </Card>
+      <Card className="container">
+        <CardTitle/>
+      </Card>
+    </div>
+    <HistoryCal />
   </div>
-  <HistoryCal />
-</div>
 );
 
 const style = {
--- client/src/components/ProgressCard.jsx
+++ client/src/components/ProgressCard.jsx
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Card, CardTitle, CardText} from 'material-ui/Card';
 import Auth from '../modules/Auth';
 
-
-
 class ProgressCard extends Component {
   constructor(props) {
     super(props);
@@ -33,30 +31,27 @@ render() {
   const newTotal = parseInt(this.state.totalAmountConsumed, 10) + parseInt(this.state.amountJustConsumed, 10);
   const amountLeft = this.state.waterGoal - this.state.totalAmountConsumed;
   return (
-    <div>
-      <h2>Today's Progress</h2>
-        <h3>
-          Your daily water goal is: {this.state.waterGoal} oz
-        </h3>
-        <h3>
-          Water currently consumed: {newTotal} oz
-        </h3>
-        <h3>
-          Amount of water to go: {amountLeft} oz
-        </h3>
-        <img src= "https://orig00.deviantart.net/bf53/f/2015/053/1/1/copinho_by_ninainlove-d8j5yty.png" alt="cute glass of water" height = "150px" width = "100px" style = {style.waterImg}/>
-    </div>
+        <div>
+          <h2>Today's Progress</h2>
+            <h3>
+              Your daily water goal is: {this.state.waterGoal} oz
+            </h3>
+            <h3>
+              Water currently consumed: {newTotal} oz
+            </h3>
+            <h3>
+              Amount of water to go: {amountLeft} oz
+            </h3>
+            <img src= "https://orig00.deviantart.net/bf53/f/2015/053/1/1/copinho_by_ninainlove-d8j5yty.png" alt="cute glass of water" height = "150px" width = "100px" style = {style.waterImg}/>
+        </div>
       )
     }
   }
 
   const style = {
     waterImg: {
-      
       align: "middle"
     }
   }
-
-
 
 export default ProgressCard;
